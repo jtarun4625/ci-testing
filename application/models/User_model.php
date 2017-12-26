f10311a0494e267c3de50ce35e101d3a715cf992
jtarun4625 <tarunjoshi4625@gmail.com>
2017-12-27 00:21:20 +0530

    Add Blog Controller and view

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -9,7 +9,6 @@ class User_model extends CI_Model
     public function getUsers()
     {
         $q = $this->db->select('firstname','lastname')
-                 ->where('id',1)
                  ->get("user_accounts");
         $result = $q->result_array();
         print_r($result);
