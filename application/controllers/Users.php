8d014317d1ca6e32db462a518d87b8ea5ddaaac6
jtarun4625 <tarunjoshi4625@gmail.com>
2017-12-26 22:47:03 +0530

    Add Blog Controller and view

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -2,8 +2,8 @@
 
 class Users extends CI_Controller {
     public function index() {
-        $this->load->model(user_model);
-        $data['users'] = $this->user_model->getUsers();
+        $this->load->model(User_model);
+        $data['users'] = $this->User_model->getUsers();
         $this->load->view(user_list,$data);
     }
 }
